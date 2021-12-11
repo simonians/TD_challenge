d4b8929c81e4fc764e19b336e900824c2651acfd
simonians <simonsimonian37@gmail.com>
2021-12-11 20:56:21 -0300

    [FIX] routes

--- src/routes/customer_route.js
+++ src/routes/customer_route.js
@@ -3,26 +3,14 @@ const router = express.Router();
 const customer_service = require("../service/customer_service")
 const url = `/customers/v1`
 
-// GET all customers
+// Routes: 
+
 router.get(`${url}/all`, customer_service.getAllCustomers)
-
-// GET A customer
 router.get(`${url}/:id`, customer_service.getCustomerById);
-
-// DELETE An Employee
-router.delete(`${url}/delete/:id`, customer_service.deleteCustomerById);
-
-// INSERT A Customer
-router.post(`${url}/create`, customer_service.createNewCustomer);
-
-// Update a customerData
-router.put(`${url}/update/:id`, customer_service.updateCustomer);
-
-// INSERT A available customer credit
-router.post(`${url}/credit/insert/:customer_id`, customer_service.insertAvailableCredit)
-
-// Get all customers order by available credit
 router.get(`${url}/all/sorted`, customer_service.getAllCustomersSortedByAvailableCredit)
-
+router.post(`${url}/create`, customer_service.createNewCustomer);
+router.post(`${url}/credit/insert/:customer_id`, customer_service.insertAvailableCredit)
+router.put(`${url}/update/:id`, customer_service.updateCustomer);
+router.delete(`${url}/delete/:id`, customer_service.deleteCustomerById);
 
 module.exports = router;
