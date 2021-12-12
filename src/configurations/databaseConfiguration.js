b6f4a270b58ca1112a1daf293bda23a2f21ebaa9
simonians <simonsimonian37@gmail.com>
2021-12-12 15:04:00 -0300

    [ADD] database connection in RDS

--- src/configurations/databaseConfiguration.js
+++ src/configurations/databaseConfiguration.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql");
 
+const fs = require("fs");
+const mysql_data = JSON.parse(fs.readFileSync('./datas/database_connection.json', "utf-8"))[0];
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "online_motorbike_shop"
+    host: mysql_data.host,
+    user: mysql_data.user,
+    password: mysql_data.password,
+    database: mysql_data.database
 })
 connection.connect((e)=> {
     if(e){
